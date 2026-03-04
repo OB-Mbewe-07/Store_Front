9df207f1948fc8ca39e3167b5f9cf66245376384
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-03-05 01:24:25 +0200

    chore(Nav) installed dependencies, added basic styli ng for functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import GetProducts from './Componets/Products'
 import { Provider } from './components/ui/provider'
+import * as React from "react";
+import {HeroUIProvider} from "@heroui/react";
+import DisplayNav from './Componets/Nav';
+import './styles/tailwind.css';
 
 function App() {
   return(
     <div>
-      <Provider>
-        <GetProducts />
-      </Provider> 
+      <HeroUIProvider>
+        <DisplayNav />
+        <Provider>
+          <GetProducts />
+        </Provider> 
+      </HeroUIProvider> 
     </div>
   )
 }
--- src/store/types.ts
+++ src/store/types.ts
@@ -7,19 +7,20 @@ export interface Props{
     description: string;
 }
 
-/*
-    Array of all products inside of the cart, the idea is that i want to have all elements that are 
-    in the cart with the quantity included inside of it
-*/
-export interface stateOfCartItems{
-    products: cartProducts[]; 
-}
- 
 export interface cartProducts{
     product: Props;
     quantity: number;
 }
 
+/*
+    Array of all products inside of the cart, the idea is that i want to have all elements that are 
+    in the cart with the quantity included inside of it
+*/
+
+export interface stateOfCartItems{
+    products: cartProducts[]; 
+}
+ 
 export type reducerActions = 
 {type: "add"; payload: Props} | 
 {type: "remove"; payload: number} | 
